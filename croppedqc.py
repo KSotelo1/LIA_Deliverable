0774a5d136486bd68770f66d781743b7aa65e551
Miadoan123 <mimid3105@gmail.com>
2025-10-16 19:24:31 -0400

    Add files via upload
    Here is the access to the data where we have cropped our dataset in order to select the columns 0,3,7. (done by Mia and Kaira)

---
+++ croppedqc.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+
+path = r"C:\Users\miado\OneDrive\Desktop\Forest_Fires_Dataset.csv"
+
+# Most likely fixes on Windows CSVs:
+df = pd.read_csv(path, usecols=[0,3,7], encoding="cp1252")
+print(df)
+
+# Exact match (case-insensitive)
+df_qc = df[df['Jurisdiction'].astype(str).str.strip().str.casefold() == 'quebec'].reset_index(drop=True)
+print(df_qc)
+
